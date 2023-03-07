8cb2d983f1daf5f912d262da94653780a1c238e3
Blai Bonet <bonetblai@gmail.com>
2023-03-07 16:50:53 -0400

    Comment out logging of sink nodes (as not relevant anymore)

--- incremental_solver_eqc.py
+++ incremental_solver_eqc.py
@@ -235,7 +235,7 @@ def solve(solver: Path,
 
                 if inst not in data['sink_nodes']:
                     sinks = pg.read_sink_nodes(distillate, logger)
-                    logger.info(f'Sinks: inst={inst}, all={sinks}')
+                    #logger.info(f'Sinks: inst={inst}, all={sinks}')
                     data['sink_nodes'][inst] = sinks
                     assert inst not in data['eq_classes']
                     data['eq_classes'][inst] = []
